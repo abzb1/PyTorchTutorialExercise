b35669e8abeb1bfbcf7b6c4d92a8658cbc11e70f
abzb1 <97136787+abzb1@users.noreply.github.com>
2022-05-30 21:44:10 +0900

    Add files via upload

--- Dataloader.py
+++ Dataloader.py
@@ -43,3 +43,43 @@ for i in range(1, cols * rows + 1):
     plt.imshow(img.squeeze(), cmap = "gray")
 #plt.show()
 plt.savefig("data.png")
+plt.close()
+
+import os 
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform = None, target_transfrom = None):
+        self.img_labels = pd.read_csv(annotations_file, names = ["file_name", "label"])
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size = 64, shuffle = True)
+test_dataloader = DataLoader(test_data, batch_size = 64, shuffle = True)
+
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape : {train_features.size()}")
+print(f"Labels batch shape : {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap = "gray")
+#plt.show()
+plt.savefig("dataset.png")
+print(f"Label : {label}")
